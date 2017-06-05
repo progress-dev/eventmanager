454d95984dc8bbc2e6d227bf5232e8ac6a8701f7
Surya Herdiyanto <suryaherdiyanto@gmail.com>
2017-06-05 17:18:40 +0800

    Penambahan migration create_events_table

---
+++ database/migrations/2017_06_05_090415_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Events',function(Blueprint $table)){
+          $table->increments('id');
+          $table->string('name');
+          $table->text('desc');
+          $table->dateTime('event_date');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Events');
+    }
+}
